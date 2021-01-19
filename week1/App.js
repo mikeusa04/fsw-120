a9367198abf786a5b82302e0af8be51f05d29e5e
mikeusa04 <71799572+mikeusa04@users.noreply.github.com>
2021-01-19 13:39:16 -0800

    week1 assignment

---
+++ week1/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox"/>
+            <p>Hello World</p>
+            
+            <input type="checkbox"/>
+            <p>Hello World</p>
+
+            <input type="checkbox"/>
+            <p>Hello World</p>
+
+            <input type="checkbox"/>
+            <p>Hello World</p>
+        </div>
+    );
+}
+export default App;
---
+++ week1/my-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+            <input type="checkbox"/>
+            <p>Hello World</p>
+            
+            <input type="checkbox"/>
+            <p>Hello World</p>
+
+            <input type="checkbox"/>
+            <p>Hello World</p>
+
+            <input type="checkbox"/>
+            <p>Hello World</p>
+        </div>
+  );
+}
+
+export default App;
